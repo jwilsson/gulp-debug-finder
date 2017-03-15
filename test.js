430f0bb92acf955c35339165ec3052c7647f1286
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2017-03-15 16:48:34 +0100

    Replace Mocha with AVA

--- test.js
+++ test.js
@@ -2,63 +2,59 @@
 
 const PluginError = require('plugin-error');
 const debugFinder = require('./');
-const assert = require('assert');
 const Stream = require('stream');
 const File = require('vinyl');
 const path = require('path');
+const test = require('ava');
 
-describe('gulp-debug-finder', () => {
-    it('should warn when there are debug statements present', (cb) => {
-        const stream = debugFinder();
+test('warn with debug statements present', (t) => {
+    const stream = debugFinder();
 
-        stream.on('error', () => {
-            assert(true);
-
-            cb();
-        });
-
-        stream.write(new File({
-            contents: new Buffer('if (true) { console.log("Hello world"); }'), // eslint-disable-line node/no-deprecated-api
-        }));
-
-        stream.end();
+    stream.on('error', () => {
+        t.pass();
     });
 
-    it('should ignore null files', () => {
-        const stream = debugFinder();
+    stream.write(new File({
+        contents: new Buffer('if (true) { console.log("Hello world"); }'), // eslint-disable-line node/no-deprecated-api
+    }));
 
+    stream.end();
+});
+
+test('ignores null files', () => {
+    const stream = debugFinder();
+
+    stream.write(new File({
+        base: __dirname,
+        contents: null,
+        path: path.join(__dirname, '/file.js'),
+    }));
+
+    stream.end();
+});
+
+test('ignores streams', (t) => {
+    const stream = debugFinder();
+
+    t.throws(() => {
         stream.write(new File({
             base: __dirname,
-            contents: null,
+            contents: new Stream(),
             path: path.join(__dirname, '/file.js'),
         }));
+    }, PluginError);
 
-        stream.end();
-    });
+    stream.end();
+});
 
-    it('should ignore streams', () => {
-        const stream = debugFinder();
+test('should throw on invalid JS files', (t) => {
+    const stream = debugFinder();
 
-        assert.throws(() => {
-            stream.write(new File({
-                base: __dirname,
-                contents: new Stream(),
-                path: path.join(__dirname, '/file.js'),
-            }));
-        }, PluginError);
+    t.throws(() => {
+        stream.write(new File({
+            contents: new Buffer('if (true) { console.log("Hello world");'), // eslint-disable-line node/no-deprecated-api
+        }));
+    }, PluginError);
 
-        stream.end();
-    });
-
-    it('should throw on invalid JS files', () => {
-        const stream = debugFinder();
-
-        assert.throws(() => {
-            stream.write(new File({
-                contents: new Buffer('if (true) { console.log("Hello world");'), // eslint-disable-line node/no-deprecated-api
-            }));
-        }, PluginError);
-
-        stream.end();
-    });
+    stream.end();
 });
